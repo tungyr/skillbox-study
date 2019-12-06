ef4525823e596a2a12f8f8e2207ceb6b36894542
tungyr <rotkivem@gmail.com>
2019-12-06 12:27:18 +0300

    first commit for skillbox study repo

---
+++ 2. Data Scientist. Аналитика. Начальный уровень/probe.py
@@ -0,0 +1,21 @@
+# user_input = input('Enter your name: ')
+# print(type(user_input))
+# print(user_input)
+
+# x, y map(int, input().split())
+# print(type(x))
+# print(x, y)
+
+# car = '345'
+# print(car.isnumeric())
+
+# S = 's\np\ta\nbbb'
+# S = r'C:\temp\new'
+# S = r'C:\semp\rew'
+# S = b'byte'
+#
+# print(S)
+
+my_list = [1, 2, 3, 'apple', ['a', 'b', 'c']]
+print(my_list)
+
