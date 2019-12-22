8d4544db1aed1a23733aed7b7a4f576e0d76b09d
tungyr <rotkivem@gmail.com>
2019-12-22 19:39:49 +0300

    while and for

--- 2. Data Scientist. Аналитика. Начальный уровень/probe.py
+++ 2. Data Scientist. Аналитика. Начальный уровень/probe.py
@@ -19,7 +19,31 @@
 # my_list = [1, 2, 3, 'apple', ['a', 'b', 'c']]
 # print(my_list)
 
-dig = list(range(1, 130))
+# else, break and continue - все вместе
+# f1, f2, count = 0, 1, 0
+# while f2 < 10000:
+#     count += 1
+#     if count > 27:
+#         print('Итераций больше чем 27. Прерываюсь.')
+#         break
+#     f1, f2 = f2, f1 + f2
+#     if f2 < 1000:
+#         continue
+#     print(f2)
+# else:
+#     print('Было', count, 'итераций')
 
-for i in dig:
-    print('120 % ', i, ' = ', 120 // i)
+i, summ = 1, 0
+while i < 10:
+    if i % 2 == 0:
+        summ = summ + i
+    i += 1
+    if summ > 500:
+        print('summ > 500!')
+        break
+    if i < 900:
+        continue
+    print(i)
+else:
+    print('finito!')
+
