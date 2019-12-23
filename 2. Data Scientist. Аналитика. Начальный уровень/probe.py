a8ec72ff7dd3f13da193500b90082d37d87132b2
tungyr <rotkivem@gmail.com>
2019-12-23 23:23:29 +0300

    some study abt packing/unpacking

--- 2. Data Scientist. Аналитика. Начальный уровень/probe.py
+++ 2. Data Scientist. Аналитика. Начальный уровень/probe.py
@@ -1,49 +1,21 @@
-# user_input = input('Enter your name: ')
-# print(type(user_input))
-# print(user_input)
+a, b = 'bic', 5
+print(a, b)
 
-# x, y map(int, input().split())
-# print(type(x))
-# print(x, y)
 
-# car = '345'
-# print(car.isnumeric())
 
-# S = 's\np\ta\nbbb'
-# S = r'C:\temp\new'
-# S = r'C:\semp\rew'
-# S = b'byte'
-#
-# print(S)
-
-# my_list = [1, 2, 3, 'apple', ['a', 'b', 'c']]
+# my_list = []
+# add = 1
+# for i in range(3):
+#     i = []
+#     for j in range(3):
+#         i.append((j+1)*add)
+#     my_list.append(i)
+#     add += 3
 # print(my_list)
-
-# else, break and continue - все вместе
-# f1, f2, count = 0, 1, 0
-# while f2 < 10000:
-#     count += 1
-#     if count > 27:
-#         print('Итераций больше чем 27. Прерываюсь.')
-#         break
-#     f1, f2 = f2, f1 + f2
-#     if f2 < 1000:
-#         continue
-#     print(f2)
-# else:
-#     print('Было', count, 'итераций')
-
-i, summ = 1, 0
-while i < 10:
-    if i % 2 == 0:
-        summ = summ + i
-    i += 1
-    if summ > 500:
-        print('summ > 500!')
-        break
-    if i < 900:
-        continue
-    print(i)
-else:
-    print('finito!')
-
+#
+#
+# # pair_list = [(1, 2), (3, 4), (5, 6)]
+#
+# for a, b, c in my_list:
+#     print(a, b, c)
+#     print(a+b+c)
