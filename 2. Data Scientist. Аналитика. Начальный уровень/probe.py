e6a3e1ae4aae8f8ae72c5400c60b0f19cffa21c9
tungyr <rotkivem@gmail.com>
2020-02-17 23:16:58 +0300

    Exceptions chapter and hometask 7.21

--- 2. Data Scientist. Аналитика. Начальный уровень/probe.py
+++ 2. Data Scientist. Аналитика. Начальный уровень/probe.py
@@ -1,32 +1,28 @@
-draft_lines = (2, 2, 0, 2, 2, 2)
-
-stores_lines = (200, 200, 200, 200, 200, 200)
-
-
-
-# self.ui.countBtn.clicked.connect(self.calculate)
-
-
-def validate_forms(draft_lines, stores_lines):
-    draft_lines = [float(draft_line) for draft_line in draft_lines]
-    for draft_line in draft_lines:
-        if draft_line not in {2, 7.8}:
-            print(draft_line, 'wrong!')
-            print("Applicable draft values only: 2 - 7.8"
-                  + "\n" +
-                  "Applicable density values only: 0.1 - 2"
-                  + "\n")
-        # warn = QMessageBox.warning(self, 'Message',
-        #                            "Applicable draft values only: 2 - 7.8"
-        #                            + "\n" +
-        #                            "Applicable density values only: 0.1 - 2"
-        #                            + "\n", QMessageBox.Ok)
-
-            return None
+def func():
+    value = input('Input an integer: ')
+    try:
+        value = int(value)
+    except ValueError as err:
+        print(f'Error: {err}, with params: {err.args}!')
+    finally:
+        return value
 
 
-    draft_lines = [float(draft_line) for draft_line in draft_lines]
-    for i in draft_lines:
-        print(type(i))
+def another_func():
+    my_float = func()
+    try:
+        my_float = float(my_float)
+    except ValueError as val_err:
+        print(f'User input is not an integer! {val_err}, {val_err.args}')
+    # finally:
+    #     my_float = 0
+    else:
+        print(f'Everythings good! {my_float}')
+    return 5 / my_float
 
-validate_forms(draft_lines, stores_lines)
+try:
+    res = another_func()
+except(ValueError, ZeroDivisionError) as final_err:
+    print(f'Something went wrong, guys! {final_err}, {final_err.args}')
+else:
+    print(res)
