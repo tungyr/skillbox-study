ff9058f5b38611830d842bbdcce263636f3f6b7b
tungyr <rotkivem@gmail.com>
2020-02-22 19:04:40 +0300

    Numpy first module, exceptions hometask

--- 2. Data Scientist. Аналитика. Начальный уровень/8. Основы Python - исключения/8.1_Exceptions/Исключения/probe.py
+++ 2. Data Scientist. Аналитика. Начальный уровень/8. Основы Python - исключения/8.1_Exceptions/Исключения/probe.py
@@ -28,9 +28,26 @@ with open('calc.txt', 'r') as ff:
 
             total += calc(line)
         except ValueError as exc:
-            print(f'Error {errors}::Cannot convert to int: {exc}, {exc.args}')
+            print(exc)
+            # print(f'Error {errors}::Cannot convert to int: {exc}, {exc.args}')
             errors += 1
             continue
 
 print('total: ', total)
 
+
+def div():
+    for i in range(2):
+        x = int(input("enter a number: "))
+        y = int(input("enter another number: "))
+        print(x, '/', y, '=', x / y)
+
+
+def sumOfPairs(L1, L2):
+    sum = 0
+    sumOfPairs = []
+    for i in range(len(L1)):
+        sumOfPairs.append(L1[i] + L2[i])
+
+
+print("sumOfPairs = ", sumOfPairs)
